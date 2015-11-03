77f8458c634a9e199b4dd95789d73b7802f3cca1
Henrik Wingerei <henrik.wingerei@bekk.no>
2015-11-03 21:42:55 +0100

    Add compile.sh to separate compile and run

---
+++ compile.sh
@@ -0,0 +1,10 @@
+mkdir -p mlib
+mkdir -p mods
+
+rm -rf mlib/*
+rm -rf mods/*
+
+javac -d mods -modulesourcepath src $(find src -name "*.java")
+jar --create --file mlib/no.bekk.mod1.jar -C mods/no.bekk.mod1 .
+jar --create --file mlib/no.bekk.mod2.jar --main-class=no.bekk.mod2.Mod2 -C mods/no.bekk.mod2 .
+
