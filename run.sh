070157e3f69ffd50e684e0ef5e831db8fe5f2d46
Henrik Wingerei <henrik.wingerei@bekk.no>
2015-11-03 21:30:14 +0100

    Adds jlink step

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ rm -rf mlib/*
 rm -rf mods/*
 
 javac -d mods -modulesourcepath src $(find src -name "*.java")
+
 jar --create --file mlib/no.bekk.api.jar -C mods/no.bekk.api .
 jar --create --file mlib/no.bekk.sok.jar -C mods/no.bekk.sok .
 jar --create --file mlib/no.bekk.twitter.jar -C mods/no.bekk.twitter .
 jar --create --file mlib/no.bekk.main.jar --main-class=no.bekk.main.Main -C mods/no.bekk.main .
-java -mp mlib -m no.bekk.main
+
+jlink --modulepath $JAVA_HOME/jmods:mlib --addmods no.bekk.main --output myapp
