5b58206adb9b5e094b3122c9f2ffb8ee4bb0b13f
Henrik Wingerei <henrik.wingerei@bekk.no>
2015-11-03 12:39:44 +0100

    Builds modular jar files

--- run.sh
+++ run.sh
@@ -4,10 +4,7 @@ mkdir -p mods
 rm -rf mlib/*
 rm -rf mods/*
 
-mkdir -p mods/no.bekk.mod1
-mkdir -p mods/no.bekk.mod2
-
-javac -d mods/no.bekk.mod1 src/no.bekk.mod1/module-info.java src/no.bekk.mod1/no/bekk/mod1/Mod1.java
-javac -modulepath mods -d mods/no.bekk.mod2 src/no.bekk.mod2/module-info.java src/no.bekk.mod2/no/bekk/mod2/Mod2.java
-
-java -modulepath mods -m no.bekk.mod2/no.bekk.mod2.Mod2
+javac -d mods -modulesourcepath src $(find src -name "*.java")
+jar --create --file mlib/no.bekk.mod1.jar -C mods/no.bekk.mod1 .
+jar --create --file mlib/no.bekk.mod2.jar --main-class=no.bekk.mod2.Mod2 -C mods/no.bekk.mod2 .
+java -mp mlib -m no.bekk.mod2
